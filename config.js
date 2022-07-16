0194beb1e4d7404c45613962d0d478e08cdb9711
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-07-16 11:31:51 +0100

    👑𝙽𝚎𝙺𝚘𝚜𝚖𝚒𝚌 - 𝙱𝙾𝚃👑

--- config.js
+++ config.js
@@ -5,14 +5,10 @@ import fs from 'fs'
 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['34642467703', '👑 Mystic - Collaborator 1 👑', true],
-  ['50499698072', '👑 Mystic - Collaborator 2 👑', true],
-  ['595986460945', '👑 Mystic - Collaborator 3 👑', true],
-  ['5219992095479'],
-  ['5219991402134'],
-  ['5219993404349'],
-  ['5219996125657']
+  ['34623442554', '👑 NeKosmic - Owner 👑', true],
+  ['', '👑 NeKosmic - Collaborator 1 👑', true],
+  ['', '👑 NeKosmic - Collaborator 2 👑', true]
+  
 ] // Cambia los numeros que quieras
 global.mods = [] 
 global.prems = [] 
