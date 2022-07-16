3ebe19ecc1412ec818378fcd442322ecb670fd80
NeKosmic-NK <104524366+NeKosmic-NK@users.noreply.github.com>
2022-07-16 12:21:48 +0100

    Update info-instalarbot.js

--- plugins/info-instalarbot.js
+++ plugins/info-instalarbot.js
@@ -1,3 +1,4 @@
+*/
 import fs from 'fs'
 let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 let text = `
@@ -35,3 +36,4 @@ sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}})
 }
 handler.command = /^(instalarbot)/i
 export default handler
+/*
